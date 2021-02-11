ebb5491cfb0f6f196d54aed7f7264737c87b67b6
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-02-12 00:59:46 +0700

    reproduce api

--- app.js
+++ app.js
@@ -1,75 +1,21 @@
-
 const express = require('express');
 const app = express()
-const scraperjs = require('scraperjs');
+const controller = require("./controller.js")
 const port = 3000
-var moment = require('moment'); 
-const fs = require('fs');
-let jsonData = require('./data.json');
-
-
 
 const enableCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', '*');
+	res.header('Access-Control-Allow-Headers', '*');
+	next();
 }
-let waktu = moment().format("DDMMYYYY");
-var current = [];
-var hasil =[];
-    scraperjs.StaticScraper.create('https://www.logammulia.com/id')
-	.scrape(function($) {
-		return $(".current").map(function() {
-			return $(this).text();
-		}).get();
-	})
-	.then(function(news) {
-        news.forEach((v,i) => {
-            if(i == 0){
-                var type = "emas";
-            }else{
-                var type = "perak";
-            }
-            current.push({date:moment().format("DD-MM-YYYY hh:mm:ss"),type:type,current_price:v.replace('Harga/gram ','')})
-        });
-        
-        scraperjs.StaticScraper.create('https://www.logammulia.com/id')
-	    .scrape(function($) {
-		    return $(".last-price").map(function() {
-		    	return $(this).text();
-		    }).get();
-	    })
-	    .then(function(news) {
-            news.forEach((v,i) => {
-                let satu =v.replace(/\t/g,'')
-                satu = satu.replace(/\n/g,'')
-                satu = satu.replace("Harga Terakhir: ",'')
-                if(i == 0){
-                    var type = "emas";
-                    current[0].last_price = satu
-                }else{
-                    var type = "perak";
-                    current[1].last_price = satu
-                }
-                
-            });
-            app.get('/',(req,res)=>{
-                res.send({
-                    data: current
-                })
-            })
-            // let data = JSON.stringify(current, null, 2);
-            // fs.writeFileSync('data.json', data);
-
-        
-        })
-})
 
 app.use(enableCrossDomain);
-console.log(current)
+app.use("/", async function(req, res, next) {
+	let data = await controller.getPrices();
 
-
+	res.send(data);
+})
 app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`)
+	console.log(`app listening at http://localhost:${port}`)
 })
